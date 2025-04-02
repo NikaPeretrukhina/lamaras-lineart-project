8afc68443d474f726f73cf8682838b59bd90be51
Alen Kocaj <alen@nimbusec.com>
2025-04-02 21:38:09 +0200

    Load pages dynamically via GraphQL from Wordpress backend and pass them onto the main component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
 import './index.css'
 import App from './App.jsx'
 
+const client = new ApolloClient({
+  uri: 'https://www.lamaras-lineart.at/graphql',
+  cache: new InMemoryCache(),
+})
+
+const GQL_PAGES = gql`
+  query {
+    pages {
+      edges {
+        node {
+          id
+          title
+          status
+        }
+      }
+    }
+  }
+`
+
+const pages = (await client.query({
+  query: GQL_PAGES
+})).data.pages.edges.map((edge) => edge.node)
+
+console.log(pages)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App page={pages[1]} />
+    </ApolloProvider>
   </StrictMode>,
 )
