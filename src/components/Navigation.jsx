e5d67fed3fc92359b7f4322974a91ff19f5d769a
Alen Kocaj <alen@nimbusec.com>
2025-05-19 21:03:32 +0200

    I'm committing this

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,31 +1,27 @@
-import { GQL_GET_MEDIA_ITEM, LOGO_SCHRIFT} from '../constants'
+import { GQL_GET_MEDIA_ITEM, LOGO_SCHRIFT } from '../constants'
 import { useGraphQL } from '../helpers'
 
 
 function Navigation() {
-    const {loading,error, data } = useGraphQL(GQL_GET_MEDIA_ITEM, {
-      variables: { id: LOGO_SCHRIFT["LogoLamara"] }
-    })
-    
- if (loading) return null; // oder: return <div>Loading...</div>
-  if (error) return <div>Error: {error.message}</div>;
-  if (!data || !data.mediaItem) return null;
+  const { loading, error, data } = useGraphQL(GQL_GET_MEDIA_ITEM, {
+    variables: { id: LOGO_SCHRIFT["LogoLamara"] }
+  })
 
-    return (
+  if (loading) return null // oder: 
+  if (error) return <div>Error: {error.message}</div>
+
+  return (
     <div className="nav">
-
-        <div className="nav-logo">
-         <img src={data.mediaItem.sourceUrl} title={data.mediaItem.title}></img> 
-        </div>
-
-        <div className="nav-links">
-          <a href="about-me">Über mich</a>
-          <a href="portfolio">Referenzen</a>
-          <a href="reviews">Bewertungen</a>
-          <a href="contsct">Termin Vereinbaren</a>
-        </div>
-        
+      {data?.mediaItem ? <div className="nav-logo">
+        <img src={data.mediaItem.sourceUrl} title={data.mediaItem.title}></img>
+      </div> : null}
+      <div className="nav-links">
+        <a href="about-me">Über mich</a>
+        <a href="portfolio">Referenzen</a>
+        <a href="reviews">Bewertungen</a>
+        <a href="contsct">Termin Vereinbaren</a>
+      </div>
     </div>
-    )
+  )
 }
 export default Navigation
