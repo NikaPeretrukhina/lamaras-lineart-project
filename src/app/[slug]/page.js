35854db9b2baea416b4fb81851b27a14c5ded52b
Alen Kocaj <alen@nimbusec.com>
2025-06-15 17:33:01 +0200

    Await the unawaitable

--- src/app/[slug]/page.js
+++ src/app/[slug]/page.js
@@ -1,6 +1,7 @@
 import { LINKS_COMPONENT_MAPPING } from "@/constants";
 
-export default async function Page({ params }) {
+export default async function Page(props) {
+    const params = await props.params
     return <div>
         {LINKS_COMPONENT_MAPPING[params.slug].component || `${params.slug}: Not yet implemented`}
     </div>
